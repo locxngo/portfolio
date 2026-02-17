c44344645c101bc54d80dcea55d1a6d4b08af181
Loc Ngo <xuanloc0511@gmail.com>
2026-02-17 14:16:04 -0500

    update next.config.ts for support gh-pages
    Signed-off-by: Loc Ngo <xuanloc0511@gmail.com>

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
+const repoName = 'portfolio';
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: "/portfolio",
+  basePath: isProd ? `/${repoName}` : '',
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  reactStrictMode: true,
   images: {
     unoptimized: true,
   },
