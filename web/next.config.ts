c60834e2ce92cb780d3d9b235b87371f17f34c8d
Loc Ngo <xuanloc0511@gmail.com>
2026-03-18 23:14:50 -0400

    remove extra package.json
    Signed-off-by: Loc Ngo <xuanloc0511@gmail.com>

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,5 +1,5 @@
 import type { NextConfig } from "next";
-const isProd = process.env.NODE_ENV === 'production';
+const isProd = process.env.DEPLOYED === 'production';
 const repoName = 'portfolio';
 
 const nextConfig: NextConfig = {
